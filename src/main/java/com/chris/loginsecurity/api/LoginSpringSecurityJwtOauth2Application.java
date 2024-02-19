bfac9772359dae0936a868d674200d3433ec788f
Chris <cristian.28hedz@gmail.com>
2024-02-18 22:55:15 -0600

    commit inicial

---
+++ src/main/java/com/chris/loginsecurity/api/LoginSpringSecurityJwtOauth2Application.java
@@ -0,0 +1,13 @@
+package com.chris.loginsecurity.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringSecurityJwtOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSpringSecurityJwtOauth2Application.class, args);
+    }
+
+}
