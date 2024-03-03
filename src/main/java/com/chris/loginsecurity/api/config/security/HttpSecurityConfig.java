6a1a12529378559b96f127fc7adf005c2fc828de
Chris <cristian.28hedz@gmail.com>
2024-03-02 18:42:40 -0600

    feat:  🔧 Agregue la configuracion de cors para el acceso de peticiones desde el cliente frontend

--- src/main/java/com/chris/loginsecurity/api/config/security/HttpSecurityConfig.java
+++ src/main/java/com/chris/loginsecurity/api/config/security/HttpSecurityConfig.java
@@ -6,17 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @AllArgsConstructor
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class HttpSecurityConfig {
 
     private JwtAuthenticationFilter jwtAuthenticationFilter;
@@ -34,7 +39,20 @@ public class HttpSecurityConfig {
                     authorizeRequest.requestMatchers(HttpMethod.POST,"api/v1/user").permitAll();
                     authorizeRequest.requestMatchers(HttpMethod.POST,"api/v1/login").permitAll();
                     authorizeRequest.requestMatchers(HttpMethod.POST,"api/v1/profile").permitAll();
+                    authorizeRequest.requestMatchers(HttpMethod.GET,"api/v1/demo").permitAll();
                     authorizeRequest.anyRequest().authenticated();
                 }).build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173/"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
